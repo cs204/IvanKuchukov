3a64792007eb972eff0f484c804ffd661f238814
test <test>
2023-10-21 13:19:19 +0700

    test

---
+++ bank.py
@@ -0,0 +1,7 @@
+user_input = input("Приветствие: ")
+if user_input == "здрасте":
+    print("$20")
+elif user_input.startswith("з"):
+    print("$0")
+elif user_input == "привет":
+    print("$100")
